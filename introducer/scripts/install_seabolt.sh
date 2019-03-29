2e1bda73f5e2942b8857e859635a51de1816a995
Maxim Zhiburt <zhiburt@gmail.com>
2019-03-29 13:43:21 +0300

    Change build script

--- introducer/scripts/install_seabolt.sh
+++ introducer/scripts/install_seabolt.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-
-export DYLD_LIBRARY_PATH=../iternal/libraries/seabolt-1.7/build/dist/lib64
-export PKG_CONFIG_PATH=../iternal/libraries/seabolt-1.7/build/dist/share/pkgconfig
-export LD_LIBRARY_PATH=../iternal/libraries/seabolt-1.7/build/dist/lib64
+export DYLD_LIBRARY_PATH=$(pwd)/iternal/libraries/seabolt-1.7/build/dist/lib64
+export PKG_CONFIG_PATH=$(pwd)/iternal/libraries/seabolt-1.7/build/dist/share/pkgconfig
+export LD_LIBRARY_PATH=$(pwd)/iternal/libraries/seabolt-1.7/build/dist/lib64
 echo $DYLD_LIBRARY_PATH
 echo $PKG_CONFIG_PATH
 echo $LD_LIBRARY_PATH
