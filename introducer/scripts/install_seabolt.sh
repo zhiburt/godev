6fdf68ec23a6c2d72ed9df2e4b5ae022be5255ab
Maxim Zhiburt <zhiburt@gmail.com>
2019-03-29 14:52:20 +0300

    Change script(test travis)

--- introducer/scripts/install_seabolt.sh
+++ introducer/scripts/install_seabolt.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+echo $DYLD_LIBRARY_PATH
+echo $PKG_CONFIG_PATH
+echo $LD_LIBRARY_PATH
 export DYLD_LIBRARY_PATH=$(pwd)/iternal/libraries/seabolt-1.7/build/dist/lib64
 export PKG_CONFIG_PATH=$(pwd)/iternal/libraries/seabolt-1.7/build/dist/share/pkgconfig
 export LD_LIBRARY_PATH=$(pwd)/iternal/libraries/seabolt-1.7/build/dist/lib64
 cd $(pwd)/iternal/libraries/seabolt-1.7/
+apt-get update cmake -y
 cmake .
 ./make_release.sh
 echo $DYLD_LIBRARY_PATH
